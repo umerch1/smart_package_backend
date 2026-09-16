9330757026b9e18bfe5e972480f6fdd0ebde3cac
Muhammad Farooq <umerf3024@gmail.com>
2026-09-16 21:02:26 +0500

    Change port webserver

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 const startServer = async () => {
   try {
